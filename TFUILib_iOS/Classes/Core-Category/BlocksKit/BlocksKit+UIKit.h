729af4babac909153eadd965c3676feed004d23b
Daniel <xiaofei.sun@ingeek.com>
2023-06-01 18:27:10 +0800

    修复BlocksKit UIEditUserWordController causes A2DynamicDelegate UITextField+BlocksKit crash

--- TFUILib_iOS/Classes/Core-Category/BlocksKit/BlocksKit+UIKit.h
+++ TFUILib_iOS/Classes/Core-Category/BlocksKit/BlocksKit+UIKit.h
@@ -1,5 +1,5 @@
 //
-//  BlocksKit+UIKit
+//  BlocksKit UIKit Extensions
 //
 //  The Objective-C block utilities you always wish you had.
 //
@@ -25,13 +25,19 @@
 //  THE SOFTWARE.
 //
 
+#import "BlocksKit.h"
+
 #import "UIActionSheet+BlocksKit.h"
 #import "UIAlertView+BlocksKit.h"
 #import "UIBarButtonItem+BlocksKit.h"
 #import "UIControl+BlocksKit.h"
 #import "UIGestureRecognizer+BlocksKit.h"
+#import "UIImage+BlocksKit.h"
+#import "UIImagePickerController+BlocksKit.h"
 #import "UIPopoverController+BlocksKit.h"
 #import "UITextField+BlocksKit.h"
 #import "UIView+BlocksKit.h"
+#import "UIWebView+BlocksKit.h"
 #import "UITextField+BlocksKit.h"
+#import "UITextView+BlocksKit.h"
 #import "UIImagePickerController+BlocksKit.h"
